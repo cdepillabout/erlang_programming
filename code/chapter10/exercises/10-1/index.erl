9f59500bd3c701ee3c185dd3387eb5dd7dc6395b
(cdep)illabout <cdep.illabout@gmail.com>
2016-11-25 00:25:58 +0900

    Completed exercise 10-1.

--- code/chapter10/exercises/10-1/index.erl
+++ code/chapter10/exercises/10-1/index.erl
@@ -9,7 +9,7 @@
 -module(index).
 
 -export([index/1,processFile/1,prettyEntry/1]).
--export([accumulate/1,removeDuplicates/1,groupSuccessors/1,createRanges/1]).
+-export([accumulate/1,removeDuplicates/1,groupSuccessors/1,createRanges/1,pad/2,prettyList/1]).
 
 index(File) ->
   ets:new(indexTable, [ordered_set, named_table]),
@@ -23,8 +23,10 @@ processFile(File) ->
 processLines(IoDevice,N) ->
   case io:get_line(IoDevice,"") of
     eof ->
+      io:format("processLines: got EOF!~n"),
       ok;
     Line ->
+      io:format("processLines: got line: ~s", [Line]),
       processLine(Line,N),
       processLines(IoDevice,N+1)
   end.
@@ -32,11 +34,13 @@ processLines(IoDevice,N) ->
 -define(Punctuation,"(\\ |\\,|\\.|\\;|\\:|\\t|\\n|\\(|\\))+").
 
 processLine(Line,N) ->
-  case re:split(Line,?Punctuation) of
-    {ok,Words} ->
-      processWords(Words,N) ;
-    _ -> []
-  end.
+  Words = doRegex(Line,?Punctuation),
+  io:format("processLine: got words: ~w!~n", [Words]),
+  processWords(Words,N).
+
+doRegex(String,Regex) ->
+  RegexRes = re:split(String, Regex),
+  lists:map(fun unicode:characters_to_list/1, RegexRes).
 
 processWords(Words,N) ->
   case Words of
@@ -45,6 +49,7 @@ processWords(Words,N) ->
       if
         length(Word) > 3 ->
           Normalise = string:to_lower(Word),
+          io:format("processWords: inserting word: ~w~n", [Normalise]),
           ets:insert(indexTable,{{Normalise , N}});
         true -> ok
       end,
@@ -55,12 +60,9 @@ prettyIndex() ->
   case ets:first(indexTable) of
     '$end_of_table' ->
       ok;
-    First  ->
-      case First of
-        {Word, N} ->
-          IndexEntry = {Word, [N]}
-      end,
-      prettyIndexNext(First,IndexEntry)
+    {Word,N} ->
+      IndexEntry = {Word, [N]},
+      prettyIndexNext({Word, N}, IndexEntry)
   end.
 
 
@@ -79,9 +81,11 @@ prettyIndexNext(Entry,{Word, Lines}=IndexEntry) ->
       end
   end.
 
-prettyEntry(IndexEntry) ->
-    % exercise
-    ok.
+prettyEntry({Word, Lines}) ->
+  % io:format("prettyEntry: Word: ~s, Lines: ~w, accumulate(Lines): ~w~n", [Word, Lines, accumulate(Lines)]),
+  % io:format("prettyEntry: prettyList(accumulate(Lines)): ~s~n", [prettyList(accumulate(Lines))]),
+  Index = pad(2, prettyList(accumulate(Lines))),
+  io:format("~s~n~s~n", [Word, Index]).
 
 accumulate(List) ->
   SortedList = lists:sort(List),
@@ -121,3 +125,18 @@ createRanges(List) -> lists:map(fun createRangesHelper/1, List).
 createRangesHelper([H]) -> {H};
 createRangesHelper([H1,H2]) -> {H1,H2};
 createRangesHelper([H1,_|T]) -> createRangesHelper([H1|T]).
+
+prettyList(List) ->
+  WithSep = lists:map(fun ({Item}) ->
+                          integer_to_list(Item);
+                          ({ItemA, ItemB}) ->
+                          integer_to_list(ItemA) ++ "-" ++ integer_to_list(ItemB)
+                      end,
+                      List),
+  Joined = string:join(WithSep, ","),
+  Joined ++ ".".
+
+pad(N, Word) when N > 0 ->
+  pad(N - 1, " " ++ Word);
+pad(_, Word) ->
+  Word.
